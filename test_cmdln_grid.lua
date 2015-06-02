2cad31474a3602d55b29f049dfddd6be73011e24
PH <djent.git@cock.li>
2015-06-02 12:34:41 -0400

    Added another project

---
+++ test_cmdln_grid.lua
@@ -0,0 +1,16 @@
+function main()
+	clear()
+	print("cleared")
+	print(" 123456789abcdefghijklmn")
+	print("1")
+	print("2")
+	print("3")
+end
+
+function clear()
+	--windows
+	os.execute("cls")
+end
+
+main()
+os.execute("pause")
