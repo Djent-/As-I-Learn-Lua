27e321f27d2b7d196ba8fd45381c07ae0cddc23e
PH <djent.git@cock.li>
2015-06-02 22:48:52 -0400

    another test project concerning file reading, updating test_cmd_grid.lua to be CRUD

--- cmdln_grid/test_cmdln_grid.lua
+++ cmdln_grid/test_cmdln_grid.lua
@@ -17,10 +17,12 @@ function main()
 	for x = 1, #line_topper do
 		test_line = test_line .. "."
 	end
+	local data = {}
 	for y = 1, 10 do
 		queue_line(test_line)
 	end
-	display()
+	--write_file(to_display)
+	display(read_file())
 end
 
 function clear()
@@ -33,16 +35,17 @@ function pause()
 	os.execute("pause")
 end
 
-function display()
+function display(table_data)
+	local table_data = table_data or to_display
 	clear()
 	--calculate the amount of spaces needed
-	local digits_in_line = getDigits(#to_display)
+	local digits_in_line = getDigits(#table_data)
 	print(string.rep(" ", digits_in_line + 1) .. line_topper)
 	
-	for x = 1, #to_display do
-		print(x .. string.rep(" ", digits_in_line - getDigits(x) + 1) .. to_display[x])
+	for x = 1, #table_data do
+		print(x .. string.rep(" ", digits_in_line - getDigits(x) + 1) .. table_data[x])
 	end
-	to_display = {}
+	table_data = {}
 end
 
 function getDigits(number)
@@ -56,6 +59,29 @@ end
 
 function queue_line(line)
 	table.insert(to_display, line)
+end
+
+function read_file(filename)
+	local filename = filename or "cmdln_grid.DATA"
+	datafile = io.open(filename, "w")
+	local data = {}
+	for line in datafile:lines() do
+		table.insert(data, line)
+	end
+	datafile:flush()
+	datafile:close()
+	return data
+end
+
+function write_file(data, filename)
+	local filename = filename or "cmdln_grid.DATA"
+	datafile = io.open(filename, "w+")
+	local data = data
+	for x = 1, #data do
+		datafile:write(data[x] .. "\n")
+	end
+	datafile:flush()
+	datafile:close()
 end
 
 to_display = {}
